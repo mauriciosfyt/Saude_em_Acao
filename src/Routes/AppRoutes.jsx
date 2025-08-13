791295ce5e5301fec0771aae50cedbe1e1612b07
João Vitor <157393407+Jaowzinho@users.noreply.github.com>
2025-08-13 11:31:02 -0300

    Add files via upload

--- src/Routes/AppRoutes.jsx
+++ src/Routes/AppRoutes.jsx
@@ -10,11 +10,14 @@ import SobreNos from '../pages/SobreNos/index.jsx';
 import LojaProduto from '../pages/LojaProduto/index.jsx';
 import SobrenosLoja from '../pages/SobreNos_loja/index.jsx';
 import Carrinho from '../pages/Carrinho/index.jsx';
-import CategoriaCreatina from '../pages/CategoriaCreatina/index.jsx';
-import CategoriaCamisa from '../pages/CategoriaCamisa/index.jsx';
 import Planos from '../pages/Planos/index.jsx';
 import Erro404 from '../pages/Erro404/erro_404.jsx';
+import ReservasEfetuadas from '../pages/Tela Adm/Reservas_Efetuada/index.jsx';
 
+import CategoriaCreatina from '../pages/CategoriaCreatina/index.jsx';
+import CategoriaCamisa from '../pages/CategoriaCamisa/index.jsx';
+import CategoriaVitaminas from '../pages/CategoriaVitaminas/index.jsx';
+import CategoriaWhey from '../pages/CategoriaWhey/index.jsx';
 
 const AppRoutes = () => {
   return (
@@ -29,10 +32,16 @@ const AppRoutes = () => {
         <Route path="/LojaProduto" element={<LojaProduto />} />
         <Route path="/SobrenosLoja" element={<SobrenosLoja />} />
         <Route path="/Carrinho" element={<Carrinho />} />
-        <Route path="/CategoriaCreatina" element={<CategoriaCreatina />} />
-        <Route path="/CategoriaCamisa" element={<CategoriaCamisa />} />
         <Route path="/Planos" element={<Planos />} />
         <Route path="*" element={<Erro404 />} /> {/* Adicione esta linha */}
+
+        <Route path="/CategoriaCreatina" element={<CategoriaCreatina />} />
+        <Route path="/CategoriaCamisa" element={<CategoriaCamisa />} />
+        <Route path="/CategoriaWhey" element={<CategoriaWhey />} />
+        <Route path="/CategoriaVitaminas" element={<CategoriaVitaminas />} />
+        <Route path="/ReservasEfetuadas" element={<ReservasEfetuadas />} /> {/* Adicione esta linha */}
+        
+        {/* Adicione outras rotas conforme necessário */}
       </Routes>
     </Router>
   );
