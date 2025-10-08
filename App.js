4e096da3e9b775cf0ac6acf40fee83e7d0cc7e29
Pedro Henrique Honorio Lima <ph1217lima@gmail.com>
2025-10-08 08:20:32 -0300

    App.js

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
-
 import React from 'react';
 import Routes from './src/Routes/Routes';
 import { FavoritosProvider } from './src/context/FavoritosContext';
+import { TreinosProvider } from './src/context/TreinosContext';
 import { ThemeProvider } from './src/context/ThemeContext';
+
 
 export default function App() {
   return (
     <FavoritosProvider>
-      <ThemeProvider>
+      <TreinosProvider>
         <Routes />
-      </ThemeProvider>
+      </TreinosProvider>
     </FavoritosProvider>
   );
 }
