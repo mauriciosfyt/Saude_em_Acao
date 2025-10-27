b884429f9824545789b8e1593c0d635a875979bb
Pedro Henrique Honorio Lima <ph1217lima@gmail.com>
2025-10-27 17:45:17 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -9,7 +9,9 @@ export default function App() {
   return (
     <FavoritosProvider>
       <TreinosProvider>
-        <Routes />
+        <ThemeProvider>
+          <Routes />
+        </ThemeProvider>
       </TreinosProvider>
     </FavoritosProvider>
   );
