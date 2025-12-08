fd8d7d033e5269ddd0cf95d3957b52878bd8b854
feliperottner <feliperottnerrodrigues@gmail.com>
2025-12-08 09:41:06 -0300

    Implementação da rota do front-end na class securitySegurancaFilterChain.

--- src/main/java/br/com/saudeemacao/api/security/SegurancaFilterChain.java
+++ src/main/java/br/com/saudeemacao/api/security/SegurancaFilterChain.java
@@ -93,7 +93,7 @@ public class SegurancaFilterChain {
                 "http://localhost:5173",
                 "http://localhost:8080",
                 "http://127.0.0.1:5500",
-                "http://100.30.34.6:8080"
+                "http://100.30.34.6:3000"
         ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "PATCH"));
         configuration.setAllowedHeaders(List.of(""));
