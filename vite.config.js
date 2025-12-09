a84eb2c8ec7d0fcdca59f2c55ac3ba64a87afbc2
João Vitor <157393407+Jaowzinho@users.noreply.github.com>
2025-12-08 23:34:31 -0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   define: {
-    'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL || 'http://98.92.159.66'),
+    'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL || 'http://54.144.210.178'),
   },
   server: {
     // Proxy /api requests to the backend to avoid CORS during development
     proxy: {
       '/api': {
-        target: 'http://98.92.159.66',
+        target: 'http://54.144.210.178',
         changeOrigin: true,
         secure: false,
       },
